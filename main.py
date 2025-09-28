223b3aa23278be8a395785c4b53c0551abac7ed0
timwelliott <metaversedeveloper@gmail.com>
2025-09-28 02:28:23 -0400

    I am almost done with the report,

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
+from stats import get_num_words
+from stats import count_each_char   
+
 def get_book_text(file_path: str) -> str:
     """Read and return the full text of the book at file_path."""
     with open(file_path, "r") as f:
          contents = f.read()
     return contents 
 
-def count_words(text: str) -> int:
-    """Count and return the number of words in the given text."""
-    words = text.split()
-    return len(words)
 
 def main():
     # Example usage
     book_path = "books/frankenstein.txt"
     book_text = get_book_text(book_path)
-    num_words = count_words(book_text)
+    num_words = get_num_words(book_text)    
     print(f"Found {num_words} total words")
-
+    char_count = count_each_char(book_text)
+    print(f"Character counts: {char_count}")
 if __name__ == "__main__":
     main()
---
+++ stats.py
@@ -0,0 +1,24 @@
+def get_num_words(text: str) -> int:  
+    """Count and return the number of words in the given text."""
+    words = text.split()
+    return len(words)
+
+def count_each_char(text: str) -> dict:
+    """Count occurrences of each character in the text and return as a dictionary."""
+    char_count = {}
+    for char in text.lower():
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
+
+def get_num_from_dict(data: dict) -> int:
+    """Return data["num"]."""
+    return data["num"]
+
+def create_sorted_list_of_dicts(data: dict) -> list:
+    """Convert a dictionary to a sorted list of dictionaries with 'char' and 'num' keys."""
+    list_of_dicts = [{"char": data[char], "num": data[num]} for char, num in data]
+    list_of_dicts  = sorted( list_of_dicts, key=get_num_from_dict reverse=True)
+    return list_of_dicts
